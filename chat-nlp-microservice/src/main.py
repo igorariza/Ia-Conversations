c9adf9e59a1c08a4b17e74ddb3c4b9bd192dcabe
igorariza <igorariza@gmail.com>
2025-02-27 21:03:41 -0500

    Set OpenAI API key from environment variable for configuration

--- chat-nlp-microservice/src/main.py
+++ chat-nlp-microservice/src/main.py
@@ -15,6 +15,7 @@ from nlp.topic_modeling import extract_topics
 from nlp.summarization import generate_summary
 
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 origins = [
     "http://localhost:4200",
